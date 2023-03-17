e11ac4aa781fe62221d5ef3203d67b825bd6e67d
xnxaa <vivimusfiroh83@gmail.com>
2023-03-17 21:25:21 +0700

    ujian

---
+++ 3/script.js
@@ -0,0 +1,14 @@
+function akses(login) {
+  const akses1 = login;
+  
+  return akses1;
+}
+
+const role = 'admin';
+
+
+if (role == 'admin'){
+  console.log("Selamat datang, Admin!")
+}else {
+  console.log("Anda tidak memiliki akses ke halaman ini!")
+}
